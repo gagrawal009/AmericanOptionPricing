f7b9b2c5b9db44cf59cd4814cb3be69c6e781272
yifandill <dillhui01@gmail.com>
2024-11-20 21:23:56 +0800

    format Algorithm 2

--- utils/algorithm2.py
+++ utils/algorithm2.py
@@ -1,15 +1,22 @@
 import numpy as np
 
+
 def chebyshev_approximation(z, a):
-    l = len(a)
-    n = l-1
-    b = np.zeros(l+1)
-    b[n+1] = 0
+    """
+        Performs Chebyshev approximation by Clenshaw algorithm.
+
+        Parameters:
+            z (float or ndarray): The point(s) at which to evaluate the Chebyshev series.
+            a (array-like): Coefficients of the Chebyshev series, ordered from degree 0 to n.
+
+        Returns:
+            float or ndarray: The value(s) of the Chebyshev series at point(s) z.
+    """
+    n = len(a) - 1
+    b = np.zeros(n + 2)
     b[n] = a[n]
-    for k in range(n-1, 0, -1):
-        b[k] = a[k] + 2*z*b[k+1] - b[k+2]
+    for k in range(n - 1, 0, -1):
+        b[k] = a[k] + 2 * z * b[k+1] - b[k+2]
     
-    fc = a[0] + b[1]*z - b[2]
+    fc = a[0] + b[1] * z - b[2]
     return fc
-
-
