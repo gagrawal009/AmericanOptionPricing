5b2a8317073916c2cc3b63c2b3467243430919cc
yifandill <dillhui01@gmail.com>
2024-11-20 20:49:36 +0800

    format numerical integrator Algorithm 1

--- utils/algorithm1.py
+++ utils/algorithm1.py
@@ -1,17 +1,81 @@
 import numpy as np
-def numerical_integration (f, l, quadrature_rule, a=-1, b=1):
-    S = 0
-    if quadrature_rule=='Gauss-Legendre':
-        u, w = np.polynomial.legendre.leggauss(l)
-        v = ((b-a)/2) * u + (a+b)/2
-        S = np.sum(w * f(v))
 
-    elif quadrature_rule=='Tanh-Sinh':
-        s = int(l/2)
-        for k in range (-s,s+1):
-            u = np.tanh((np.pi/2) * np.sinh(k))
-            w = (np.pi/2 * np.cosh(k)) / ((np.cosh((np.pi/2) * np.sinh(k)))**2)
-            v = ((b-a)/2) * u + (a+b)/2
-            S = S + w * f(v)
 
-    return ((b-a)/2)*S
+class NumericalIntegrator:
+    """
+    A class for performing numerical integration using various quadrature rules.
+    """
+
+    def __init__(self, function, a: float = -1.0, b: float = 1.0):
+        """
+        Initializes the NumericalIntegrator with the function to integrate and the integration bounds.
+
+        Parameters:
+            function (callable): The function to integrate.
+            a (float, optional): Lower bound of integration. Default is -1.0.
+            b (float, optional): Upper bound of integration. Default is 1.0.
+        """
+        self.function = function
+        self.a = a
+        self.b = b
+
+    def integrate(self, n_points: int, method: str = 'Gauss-Legendre') -> float:
+        """
+        Performs numerical integration using the specified quadrature rule.
+
+        Parameters:
+            n_points (int): Number of integration points.
+            method (str, optional): The quadrature rule to use ('Gauss-Legendre' or 'Tanh-Sinh').
+                                    Default is 'Gauss-Legendre'.
+
+        Returns:
+            float: The numerical approximation of the integral.
+        """
+        if method == 'Gauss-Legendre':
+            return self._gauss_legendre(n_points)
+        elif method == 'Tanh-Sinh':
+            return self._tanh_sinh(n_points)
+        else:
+            raise ValueError("Unsupported method. Choose 'Gauss-Legendre' or 'Tanh-Sinh'.")
+
+    def _gauss_legendre(self, n_points: int) -> float:
+        """
+        Private method to perform Gauss-Legendre quadrature.
+
+        Parameters:
+            n_points (int): Number of integration points.
+
+        Returns:
+            float: The numerical approximation of the integral.
+        """
+        nodes, weights = np.polynomial.legendre.leggauss(n_points)
+        # Map the nodes from [-1, 1] to [a, b]
+        transformed_nodes = 0.5 * (self.b - self.a) * nodes + 0.5 * (self.a + self.b)
+        result = np.dot(weights, self.function(transformed_nodes))
+        return 0.5 * (self.b - self.a) * result
+
+    def _tanh_sinh(self, n_points: int, h: float = 1.0) -> float:
+        """
+        Private method to perform Tanh-Sinh quadrature.
+
+        Parameters:
+            n_points (int): Number of integration points.
+            h (float, optional): Step size can be adjusted as needed. Default is 1.0.
+
+        Returns:
+            float: The numerical approximation of the integral.
+        """
+        s = n_points // 2
+        k_values = np.arange(-s, s + 1)
+        t = k_values * h
+        sinh_t = np.sinh(t)
+        cosh_t = np.cosh(t)
+        pi_over_2 = np.pi / 2
+
+        u = np.tanh(pi_over_2 * sinh_t)
+        w = (pi_over_2 * h * cosh_t) / (np.cosh(pi_over_2 * sinh_t) ** 2)
+
+        # Map the nodes from [-1, 1] to [a, b]
+        transformed_nodes = 0.5 * (self.b - self.a) * u + 0.5 * (self.a + self.b)
+        result = np.dot(w, self.function(transformed_nodes))
+        return 0.5 * (self.b - self.a) * result
