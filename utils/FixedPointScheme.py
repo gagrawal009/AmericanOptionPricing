c76593ff50224a23a24df0ba5594dbeb1cef6a16
yifandill <dillhui01@gmail.com>
2024-11-25 22:56:38 +0800

    adjust step size h for Tanh-Sinh method

--- utils/FixedPointScheme.py
+++ utils/FixedPointScheme.py
@@ -1,11 +1,12 @@
 import numpy as np
 from scipy.stats import norm
-from utils.Integration import NumericalIntegrator
-from utils.Chebyshev import interpolate_B
+from Integration import NumericalIntegrator
+from Chebyshev import interpolate_B
+from joblib import Parallel, delayed
 
 
 class FixedPointIterator:
-    def __init__(self, sigma, r, q, K, a, tau_max, B_tau0, tau, B_tau, n_points, method):
+    def __init__(self, sigma, r, q, K, a, tau_max, B_tau0, tau_array, B_tau_array, n_points, method):
         """
         Initializes the fixed point iterator class with common parameters.
 
@@ -17,8 +18,8 @@ class FixedPointIterator:
             a (array-like): Coefficients of the Chebyshev series.
             tau_max (float): The maximum horizon of the interval.
             B_tau0 (float): The initial value of B at tau0.
-            tau (float): Specific tau value.
-            B_tau (float): Boundary value at tau.
+            tau_array (array-like): Array of tau values.
+            B_tau_array (array-like): Array of boundary values at tau.
             n_points (int): Number of integration points.
             method (str): Integration method ('Gauss-Legendre', 'Tanh-Sinh').
         """
@@ -29,22 +30,23 @@ class FixedPointIterator:
         self.a = a
         self.tau_max = tau_max
         self.B_tau0 = B_tau0
-        self.tau = tau
-        self.B_tau = B_tau
+        self.tau_array = np.array(tau_array)
+        self.B_tau_array = np.array(B_tau_array)
         self.n_points = n_points
         self.method = method
 
-    def d_plus_minus(self, s: float, z: float, sign: int):
+
+    def d_plus_minus(self, s, z, sign):
         """
-        Calculates the d+ or d- value used in option pricing formulas.
+        Vectorized calculation of d+ or d- values.
 
         Parameters:
-            s (float): Time to maturity.
-            z (float): Normally a ratio of two prices.
+            s (array-like): Time to maturity.
+            z (array-like): Normally a ratio of two prices.
             sign (int): +1 for d+, -1 for d-.
 
         Returns:
-            float: The calculated d+ or d- value.
+            ndarray: The calculated d+ or d- values.
         """
         log_term = np.log(z)
         drift = (self.r - self.q + sign * 0.5 * self.sigma**2) * s
@@ -52,117 +54,127 @@ class FixedPointIterator:
 
         return (log_term + drift) / denominator
 
-    def kappa1(self):
+    def kappa1(self, tau, B_tau):
         """
-        Calculates the kappa1 function.
+        Calculates the kappa1 function for a given tau and B_tau.
 
         Returns:
             float: The value of kappa1.
         """
         def integrand(y):
-            t = self.tau * (1 + y)**2 / 4
-            t_diff = self.tau - t
+            t = tau * (1 + y)**2 / 4
+            t_diff = tau - t
+            t_diff = np.maximum(1e-10, t_diff)
             exp_term = np.exp(-self.q * t)
             B_tau_minus_t = interpolate_B(tau=t_diff, a=self.a, tau_max=self.tau_max, B_tau0=self.B_tau0)
-            B_ratio = self.B_tau / B_tau_minus_t
+            B_ratio = B_tau / B_tau_minus_t
             d_plus = self.d_plus_minus(s=t, z=B_ratio, sign=1)
             return exp_term * (1 + y) * norm.cdf(d_plus)
 
         integrator = NumericalIntegrator(function=integrand, a=-1, b=1)
         I = integrator.integrate(n_points=self.n_points, method=self.method)
 
-        return 0.5 * self.tau * np.exp(self.q * self.tau) * I
+        return 0.5 * tau * np.exp(self.q * tau) * I
 
-    def kappa2(self):
+    def kappa2(self, tau, B_tau):
         """
-        Calculates the kappa2 function.
+        Calculates the kappa2 function for a given tau and B_tau.
 
         Returns:
             float: The value of kappa2.
         """
         def integrand(y):
-            t = self.tau * (1 + y)**2 / 4
-            t_diff = self.tau - t
+            t = tau * (1 + y)**2 / 4
+            t_diff = tau - t
+            t_diff = np.maximum(1e-10, t_diff)
             exp_term = np.exp(-self.q * t)
             B_tau_minus_t = interpolate_B(tau=t_diff, a=self.a, tau_max=self.tau_max, B_tau0=self.B_tau0)
-            B_ratio = self.B_tau / B_tau_minus_t
+            B_ratio = B_tau / B_tau_minus_t
             d_plus = self.d_plus_minus(s=t, z=B_ratio, sign=1)
             return exp_term / self.sigma * norm.pdf(d_plus)
 
         integrator = NumericalIntegrator(function=integrand, a=-1, b=1)
         I = integrator.integrate(n_points=self.n_points, method=self.method)
 
-        return np.exp(self.q * self.tau) * np.sqrt(self.tau) * I
+        return np.exp(self.q * tau) * np.sqrt(tau) * I
 
-    def kappa3(self):
+    def kappa3(self, tau, B_tau):
         """
-        Calculates the kappa3 function.
+        Calculates the kappa3 function for a given tau and B_tau.
 
         Returns:
             float: The value of kappa3.
         """
         def integrand(y):
-            t = self.tau * (1 + y)**2 / 4
-            t_diff = self.tau - t
+            t = tau * (1 + y)**2 / 4
+            t_diff = tau - t
+            t_diff = np.maximum(1e-10, t_diff)
             exp_term = np.exp(-self.r * t)
             B_tau_minus_t = interpolate_B(tau=t_diff, a=self.a, tau_max=self.tau_max, B_tau0=self.B_tau0)
-            B_ratio = self.B_tau / B_tau_minus_t
+            B_ratio = B_tau / B_tau_minus_t
             d_minus = self.d_plus_minus(s=t, z=B_ratio, sign=-1)
             return exp_term / self.sigma * norm.pdf(d_minus)
 
         integrator = NumericalIntegrator(function=integrand, a=-1, b=1)
         I = integrator.integrate(n_points=self.n_points, method=self.method)
 
-        return np.exp(self.r * self.tau) * np.sqrt(self.tau) * I
+        return np.exp(self.r * tau) * np.sqrt(tau) * I
     
-    def f_tau(self, Jacobi_Newton: bool = True):
+    def f_tau_single(self, tau, B_tau, Jacobi_Newton=True):
         """
-        Calculates the f and f_prime functions given tau.
-
-        Parameters:
-            Jacobi_Newton (bool): If True, use Jacobi_Newton scheme; else, use ordinary Richardson scheme.
+        Calculates the f and f_prime functions for a single tau and B_tau.
 
         Returns:
             float, float: The value of f and f_prime.
         """
-        kappa1 = self.kappa1()
-        kappa2 = self.kappa2()
-        kappa3 = self.kappa3()
+        tau = np.maximum(1e-10, tau)
+        kappa1 = self.kappa1(tau, B_tau)
+        kappa2 = self.kappa2(tau, B_tau)
+        kappa3 = self.kappa3(tau, B_tau)
 
-        ratio = self.B_tau / self.K
-        d_minus = self.d_plus_minus(s=self.tau, z=ratio, sign=-1)
-        d_plus = self.d_plus_minus(s=self.tau, z=ratio, sign=1)
+        ratio = B_tau / self.K
+        d_minus = self.d_plus_minus(s=tau, z=ratio, sign=-1)
+        d_plus = self.d_plus_minus(s=tau, z=ratio, sign=1)
 
-        N_tau = norm.pdf(d_minus) / (self.sigma * np.sqrt(self.tau)) + self.r * kappa3
-        D_tau = norm.cdf(d_plus) + norm.pdf(d_plus) / (self.sigma * np.sqrt(self.tau)) + self.q * (kappa1 + kappa2)
+        N_tau = norm.pdf(d_minus) / (self.sigma * np.sqrt(tau)) + self.r * kappa3
+        D_tau = norm.cdf(d_plus) + norm.pdf(d_plus) / (self.sigma * np.sqrt(tau)) + self.q * (kappa1 + kappa2)
 
-        coefficient = self.K * np.exp(-(self.r - self.q) * self.tau)
-        f_tau = coefficient * self.N_tau() / self.D_tau()
+        coefficient = self.K * np.exp(-(self.r - self.q) * tau)
+        f_tau = coefficient * N_tau / D_tau
 
         f_prime_tau = 0
         if Jacobi_Newton:
-            N_prime_tau = -d_minus * norm.pdf(d_minus) / (self.tau * self.B_tau * self.sigma**2)
-            D_prime_tau = -d_minus * norm.pdf(d_plus) / (self.tau * self.B_tau * self.sigma**2)
+            N_prime_tau = -d_minus * norm.pdf(d_minus) / (tau * B_tau * self.sigma**2)
+            D_prime_tau = norm.pdf(d_plus) * (self.sigma * np.sqrt(tau) - d_plus) / (tau * B_tau * self.sigma**2)
             f_prime_tau = coefficient * (N_prime_tau / D_tau - (D_prime_tau * N_tau) / (D_tau**2))
 
         return f_tau, f_prime_tau
     
-    def iteration(self, Jacobi_Newton: bool = True, eta: float = 1.0):
+    def iteration(self, Jacobi_Newton=True, eta=1.0, n_jobs=1):
         """
-        Calculates the iterated value of B_tau using the Jacobi-Newton scheme.
+        Calculates the iterated values of B_tau using the Jacobi-Newton scheme for all tau values.
 
         Parameters:
-            Jacobi_Newton (bool): If True, use Jacobi_Newton scheme; else, use ordinary Richardson scheme.
-            eta (float): hyper-parameter for the iteration scheme.
+            Jacobi_Newton (bool): If True, use Jacobi-Newton scheme; else, use ordinary Richardson scheme.
+            eta (float): Hyper-parameter for the iteration scheme.
+            n_jobs (int): Number of parallel jobs.
 
         Returns:
-            float: The iterated value of B_tau.
+            ndarray: The iterated values of B_tau.
         """
+        # Function to process a single tau and B_tau
+        def process_single(tau, B_tau):
+            f_tau, f_prime_tau = self.f_tau_single(tau, B_tau, Jacobi_Newton=Jacobi_Newton)
+            step_size = (B_tau - f_tau) / (f_prime_tau - 1 + 1e-8)  # Added small epsilon to avoid division by zero
 
-        f_tau, f_prime_tau = self.f_tau(Jacobi_Newton=Jacobi_Newton)
-        step_size = (self.B_tau - f_tau) / (f_prime_tau - 1)
+            # # TODO: Tuning for hyper-parameter eta.
+            # eta = 1.0
 
-        # TODO: Tuning for hyper-parameter eta.
-        eta = 1.0
-        
-        return self.B_tau + eta * step_size
+            return B_tau + eta * step_size
+
+        # Parallel processing
+        results = Parallel(n_jobs=n_jobs)(
+            delayed(process_single)(tau, B_tau) for tau, B_tau in zip(self.tau_array, self.B_tau_array)
+        )
+
+        return np.array(results)
